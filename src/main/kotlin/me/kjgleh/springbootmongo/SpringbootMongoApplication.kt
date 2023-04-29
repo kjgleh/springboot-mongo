a570b96327519e83b35ce1ff6f7d7a4a69aa6a90
김정근 <kjg.leh@gmail.com>
2023-04-29 22:27:43 +0900

    init

---
+++ src/main/kotlin/me/kjgleh/springbootmongo/SpringbootMongoApplication.kt
@@ -0,0 +1,11 @@
+package me.kjgleh.springbootmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootMongoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootMongoApplication>(*args)
+}
---
+++ src/test/kotlin/me/kjgleh/springbootmongo/SpringbootMongoApplicationTests.kt
@@ -0,0 +1,12 @@
+package me.kjgleh.springbootmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootMongoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
